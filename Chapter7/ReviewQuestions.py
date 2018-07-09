431996842eab163a139b97ace197c61da792a671
濱口和希 <hamaguchi.timecapsule@gmail.com>
2018-07-09 19:08:08 +0900

    [end]7-3

--- Chapter7/ReviewQuestions.py
+++ Chapter7/ReviewQuestions.py
@@ -9,4 +9,11 @@ pop_bytes = mystery.encode('utf-8')
 print(pop_bytes)
 
 #7-3
+pop_string = pop_bytes.decode('utf-8')
+print(pop_string)
+
+if pop_string == mystery:
+    print('pop_string = mystery')
+
+
 
